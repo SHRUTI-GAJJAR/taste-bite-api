db493b306ab8dc85540886b3654d9f2cbf049608
SHRUTI-GAJJAR <shrutiujeniya6@gmail.com>
2025-04-29 19:01:48 +0530

    allow for use api

--- index.js
+++ index.js
@@ -17,14 +17,16 @@ const allowedOrigins = [
   
   app.use(cors({
     origin: function (origin, callback) {
-      // allow requests with no origin like mobile apps or curl
       if (!origin) return callback(null, true);
       if (allowedOrigins.includes(origin)) {
         return callback(null, true);
       } else {
         return callback(new Error('Not allowed by CORS'));
       }
-    }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
   }));
   
 // Middleware for parsing incoming JSON
