7f9a6d8d99ac88fdefba40e4bcf9f22f18a772d8
SHRUTI-GAJJAR <shrutiujeniya6@gmail.com>
2025-04-30 12:21:39 +0530

    add properties

--- models/food-items.js
+++ models/food-items.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 const foodItemSchema = new mongoose.Schema({
     name: { type: String, required: true },
     price: { type: Number, required: true },
-    Isvage: { type: Boolean, required: true }, // Assuming this is a boolean, adjust if needed
-    categories: { type: String, required: true }, // Adjust this if categories should be something else
+    Isvage: { type: Boolean, required: true }, 
+    categories: { type: String, required: true }, 
     Thumbnail_img: { type: String },
-    rating: { type: Number, min: 0, max: 5, default: 0 }
+    rating: { type: Number, min: 0, max: 5, default: 0 },
+    cookingTime: {type: String},
+    description: {type: String}
 });
 
 const Items = mongoose.model('FoodItem', foodItemSchema);
