7e0358beb4c871bfd9d5ac411db6e39b5e1f918e
SHRUTI-GAJJAR <shrutiujeniya6@gmail.com>
2025-05-01 13:05:42 +0530

    remove some properties

--- models/food-items.js
+++ models/food-items.js
@@ -2,13 +2,11 @@ const mongoose = require("mongoose");
 
 const foodItemSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  price: { type: Number, required: true },
   Isvage: { type: Boolean, required: true },
   categories: { type: String, required: true },
   Thumbnail_img: { type: String },
   rating: { type: Number, min: 0, max: 5, default: 0 },
-  cookingTime: { type: String },
-  description: { type: String },
+  cookingTime: { type: String }
 });
 
 const Items = mongoose.model("FoodItem", foodItemSchema);
